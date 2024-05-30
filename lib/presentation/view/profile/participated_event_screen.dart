8040fb6057a602ad73c1db923de06715a4945113
kurtyoon <akakertcobain@naver.com>
2024-05-30 12:17:21 +0900

    Feat: Participated Event Screen

---
+++ lib/presentation/view/profile/participated_event_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ownsaemiro/core/screen/base_screen.dart';
+import 'package:ownsaemiro/presentation/view_model/profile/participated_event_view_model.dart';
+import 'package:ownsaemiro/presentation/widget/appbar/default_back_appbar.dart';
+
+class ParticipatedEventScreen extends BaseScreen<ParticipatedEventViewModel> {
+  const ParticipatedEventScreen({super.key});
+
+  @override
+  PreferredSizeWidget buildAppBar(BuildContext context) {
+    return const PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: DefaultBackAppBar(
+          title: "참여한 행사",
+        ));
+  }
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return const Center(
+      child: Text("Participated event Screen"),
+    );
+  }
+}
