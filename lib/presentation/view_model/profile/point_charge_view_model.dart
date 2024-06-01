9a0351eb4d8e9e8508c55f599c068d8faab9dc27
Yun-Changseop <akakertcobain@naver.com>
2024-06-01 16:23:16 +0900

    ✨ Feature - Point Charge Screen
    - Feat: Point Charge Screen

--- lib/presentation/view_model/profile/point_charge_view_model.dart
+++ lib/presentation/view_model/profile/point_charge_view_model.dart
@@ -1,3 +1,31 @@
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
-class PointChargeViewModel extends GetxController {}
+class PointChargeViewModel extends GetxController {
+  var point = 0.obs;
+  final numberFormat = NumberFormat("#,###");
+
+  void addDigit(int digit) {
+    if (point.value == 0) {
+      point.value = digit;
+    } else {
+      point.value = int.parse("${point.value}$digit");
+    }
+  }
+
+  void removeDigit() {
+    if (point.value > 9) {
+      point.value = int.parse(point.value
+          .toString()
+          .substring(0, point.value.toString().length - 1));
+    } else if (point.value > 0 && point.value < 10) {
+      point.value = 0;
+    }
+  }
+
+  void clear() {
+    point.value = 0;
+  }
+
+  String get formattedPoint => numberFormat.format(point.value);
+}
