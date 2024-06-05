d70eff58cf8f15b9ddc315982b0b2bac3a503fb2
kurtyoon <akakertcobain@naver.com>
2024-06-06 01:39:49 +0900

    Feat: Market Screen Data Binding in Local

--- lib/presentation/view_model/market/market_detail_view_model.dart
+++ lib/presentation/view_model/market/market_detail_view_model.dart
@@ -1,3 +1,55 @@
 import 'package:get/get.dart';
+import 'package:ownsaemiro/data/model/market/ticket_detail_state.dart';
+import 'package:ownsaemiro/data/repository/market/market_repository.dart';
 
-class MarketDetailViewModel extends GetxController {}
+class MarketDetailViewModel extends GetxController {
+  /* ------------------------------------------------------ */
+  /* -------------------- DI Fields ----------------------- */
+  /* ------------------------------------------------------ */
+  late final MarketRepository _marketRepository;
+
+  /* ------------------------------------------------------ */
+  /* ----------------- Private Fields --------------------- */
+  /* ------------------------------------------------------ */
+  late final Rx<TicketDetailState> _ticketDetailState;
+  late final RxBool _isStateLoading = false.obs;
+
+  /* ------------------------------------------------------ */
+  /* ----------------- Public Fields ---------------------- */
+  /* ------------------------------------------------------ */
+  TicketDetailState get ticketDetailState => _ticketDetailState.value;
+
+  bool get isStateLoading => _isStateLoading.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    // Dependency Injection
+    _marketRepository = Get.find<MarketRepository>();
+
+    // Initialize State
+    _ticketDetailState = TicketDetailState(
+            id: 0,
+            title: "",
+            image: "https://i.esdrop.com/d/f/hhaNifrpr0/U3CCAUKVbb.png",
+            category: "",
+            durationTime: "",
+            rating: "",
+            address: "",
+            phoneNumber: "",
+            date: "",
+            description: "",
+            isLiked: false)
+        .obs;
+  }
+
+  void setMarketDetailInfoState(int id) {
+    _isStateLoading.value = true;
+    _marketRepository.getTicketDetailInfo(id: id).then((value) {
+      _ticketDetailState.value = value;
+    });
+
+    _isStateLoading.value = false;
+  }
+}
--- lib/presentation/view_model/market/market_view_model.dart
+++ lib/presentation/view_model/market/market_view_model.dart
@@ -1,19 +1,71 @@
 import 'package:get/get.dart';
+import 'package:ownsaemiro/app/type/e_event_category.dart';
+import 'package:ownsaemiro/data/model/market/ticket_state.dart';
+import 'package:ownsaemiro/data/repository/market/market_repository.dart';
 
 class MarketViewModel extends GetxController {
+  /* ------------------------------------------------------ */
+  /* -------------------- DI Fields ----------------------- */
+  /* ------------------------------------------------------ */
+  late final MarketRepository _marketRepository;
+
+  /* ------------------------------------------------------ */
+  /* ----------------- Private Fields --------------------- */
+  /* ------------------------------------------------------ */
+  late final RxList<TicketState> _ticketList;
+  late final Rx<EEventCategory> _eventCategory;
+
+  /* ------------------------------------------------------ */
+  /* ----------------- Public Fields ---------------------- */
+  /* ------------------------------------------------------ */
+  List<TicketState> get ticketList => _ticketList;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    // Dependency Injection
+    _marketRepository = Get.find<MarketRepository>();
+
+    // Initialize State
+    _eventCategory = EEventCategory.all.obs;
+    _ticketList = <TicketState>[].obs;
+  }
+
+  @override
+  void onReady() async {
+    super.onReady();
+
+    await _marketRepository
+        .getTicketList(page: 1, size: 8, filter: EEventCategory.all)
+        .then((value) {
+      _ticketList.addAll(value);
+    });
+  }
+
   final RxInt selectedIndex = 0.obs;
 
   final List<String> chipList = [
     "전체",
     "뮤지컬",
     "전시",
-    "행사",
     "연극",
     "콘서트",
     "스포츠",
   ];
 
-  void selectCategory(int index) {
+  void selectCategory(int index) async {
     selectedIndex.value = index;
+    String category = chipList[index];
+
+    _eventCategory.value = EEventCategory.fromKoName(category);
+
+    _ticketList.clear();
+
+    await _marketRepository
+        .getTicketList(page: 1, size: 8, filter: _eventCategory.value)
+        .then((value) {
+      _ticketList.addAll(value);
+    });
   }
 }
