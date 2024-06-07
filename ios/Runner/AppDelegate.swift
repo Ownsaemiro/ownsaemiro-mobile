c85a0d6719d8e5a28b0ac3ba7a854218f71f9fb2
kurtyoon <akakertcobain@naver.com>
2024-06-08 03:35:57 +0900

    Feat: Kakao Social OAuth

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import NaverThirdPartyLogin
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,34 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+      NaverThirdPartyLoginConnection.getSharedInstance()?.isNaverAppOauthEnable = true
+      NaverThirdPartyLoginConnection.getSharedInstance()?.isInAppOauthEnable = true
+      
+      
+      let thirdConn = NaverThirdPartyLoginConnection.getSharedInstance()
+//      thirdConn?.serviceUrlScheme = kServiceAppUrlScheme
+//      thirdConn?.consumerKey = kConsumerKey
+//      thirdConn?.consumerSecret = kConsumerSecret
+//      thirdConn?.appName = kServiceAppName
+      thirdConn?.serviceUrlScheme="ownsaemirourlscheme"
+      thirdConn?.consumerKey="X5Tnrn15UA2KjNC86fa3"
+      thirdConn?.consumerSecret="8kfDxJM9Ph"
+      thirdConn?.appName="Ownsaemiro"
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var applicationResult = false
+        if (!applicationResult) {
+           applicationResult = NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+        }
+        // if you use other application url process, please add code here.
+        
+        if (!applicationResult) {
+           applicationResult = super.application(app, open: url, options: options)
+        }
+        return applicationResult
+    }
 }
